3d8cfe30d4d21be4b69e27cbe2559a69d99c0c59
huzaifa10x <118267008+muhammadhuzaifakhanwaseem@users.noreply.github.com>
2025-03-12 02:45:50 +0500

    push

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -491,7 +491,7 @@
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script> --}}
 
     <!-- JQuery (Defer Load) -->
-    <script defer src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 
     <!-- Bootstrap JS (Defer Load) -->
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
